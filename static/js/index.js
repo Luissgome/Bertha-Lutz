54162e85bf66482b9c2066501efd8eac8260317d
Luissgome <luishenrique.souzagomes10@gmail.com>
2026-05-06 10:04:19 -0300

    index.js modificado

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,18 @@
-let verificacao = false
-function criarSala() {
+let verificacao = false;
+function entrarSala() {
     if (verificacao == false) {
         let codigo = document.createElement('input');
+        let acessarCodigo = document.createElement('button');
+        acessarCodigo.innerHTML = '<button onclick="acessar()" id="acessarId">Acessar</button>';
         codigo.innerHTML = '<input type="text" name="nome" placeholder="Seu nome">';
         document.body.appendChild(codigo);
+        document.body.appendChild(acessarCodigo);
         verificacao = true;
     }
+}
+
+function acessar() {
+    let botaoAcessar = document.getElementById('acessarId');
+    let codigo = document.querySelector('input');
+    codigo.value = '';
 }
